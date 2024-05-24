38f57870cd7426cbcf47b619f8a118e811c53f98
sebastianfabrizio <sebasotaradiaz@gmail.com>
2024-05-23 20:31:45 -0500

    Iniciando proyecto

---
+++ src/main/java/com/example/WAEC2Grupo2/Waec2Grupo2Application.java
@@ -0,0 +1,13 @@
+package com.example.WAEC2Grupo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Waec2Grupo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Waec2Grupo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WAEC2Grupo2/Waec2Grupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WAEC2Grupo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Waec2Grupo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
